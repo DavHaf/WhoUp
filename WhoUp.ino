df80e101e253d9d4c52afafc69e444c942b623c1
David <david@macbook.lan>
2020-12-30 12:44:12 -0600

    Refine colors

--- WhoUp.ino
+++ WhoUp.ino
@@ -1,5 +1,5 @@
-#define COLOR_MAX (255)
-#define NUM_STATES (7)
+#define NUM_STATES (8)
+#define DELAY (1000)
 
 struct Color {
   unsigned char r;
@@ -13,23 +13,25 @@ struct StateColor {
   bool active;
 };
 
-struct Color off = {0, 0, 0};
-struct Color red = {COLOR_MAX, 0, 0};
-struct Color green = {0, COLOR_MAX, 0};
-struct Color blue = {0, 0, COLOR_MAX};
-struct Color cyan = {0, COLOR_MAX, COLOR_MAX};
-struct Color magenta = {COLOR_MAX, 0, COLOR_MAX};
-struct Color yellow = {COLOR_MAX, COLOR_MAX, 0};
-struct Color white = {COLOR_MAX, COLOR_MAX, COLOR_MAX};
+const struct Color off =     {0x00, 0x00, 0x00};
+const struct Color red =     {0xFF, 0x00, 0x00};
+const struct Color green =   {0x00, 0xFF, 0x00};
+const struct Color blue =    {0x00, 0x00, 0xFF};
+const struct Color cyan =    {0x00, 0xFF, 0xFF};
+const struct Color magenta = {0xFF, 0x00, 0xFF};
+const struct Color yellow =  {0xFF, 0x40, 0x00};
+const struct Color white =   {0xFF, 0x60, 0x60};
+const struct Color pink =    {0xFF, 0x10, 0x10};
 
 struct StateColor states[NUM_STATES] = {
-  {red, 'r', false},
-  {green, 'g', false},
-  {blue, 'b', false},
-  {cyan, 'c', false},
+  {red,     'r', false},
+  {green,   'g', false},
+  {blue,    'b', false},
+  {cyan,    'c', false},
   {magenta, 'm', false},
-  {yellow, 'y', false},
-  {white, 'w', false}
+  {yellow,  'y', false},
+  {white,   'w', true},
+  {pink,    'p', false},
 }; 
 
 void setup() {
@@ -38,36 +40,33 @@ void setup() {
 
 int i = 0;
 bool hasColor = false;
+
 void loop() {
   if (states[i].active) {
     updateLed(states[i].color);
     hasColor = true;
-    delay(1000);
+    delay(DELAY);
   }
 
   i++;
-  if (i == NUM_STATES) {
+  if (i >= NUM_STATES) {
     if (!hasColor) {
       updateLed(off);
-      delay(1000);
+      delay(DELAY);
     }
     hasColor = false;
     i = 0;
   }
-  i = (i + 1) % NUM_STATES;
 }
 
 void serialEvent() {
   while (Serial.available()) {
     char incomingByte = (char) Serial.read();
     int j = 0;
+    char byteLower = tolower(incomingByte);
     for (j = 0; j < NUM_STATES; j++) {
-      if (states[j].name == tolower(incomingByte)) {
-        if (states[j].name == incomingByte) {
-          states[j].active = true;
-        } else {
-          states[j].active = false;
-        }
+      if (states[j].name == byteLower) {
+        states[j].active = states[j].name == incomingByte;
       }
     } 
   }
