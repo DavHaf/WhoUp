800179107d7d8e61dd534508f3e9645b5a540164
David <davidkhafner@gmail.com>
2021-01-01 04:34:43 +0000

    Communicate state with serial port

--- teamspeakLed.py
+++ teamspeakLed.py
@@ -2,30 +2,57 @@
 
 import subprocess
 import re
+import serial
+import time
+
+# None means hidden
+users = {
+  '138': None, # Admin
+  '118': b'y' # me
+}
+
+connected = list()
+
+def updateLed():
+  ser.write(b'-')
+  print(connected)
+  for id in connected:
+    #if known
+    if id in users:
+      # if the user is not hidden
+      if users[id]:
+        ser.write(users[id])
+    # write magenta for unknown users
+    else:
+      ser.write(b'm')
+
+
+ser = serial.Serial('/dev/ttyACM0')
+ser.close()
+ser.open()
+# Wait and clear
+time.sleep(3)
+ser.write(b'-')
 
 connRegex = re.compile("\|client connected '.*'\(id:(\d+)\) from \d+\.\d+\.\d+\.\d+:\d+$")
 discRegex = re.compile("\|client disconnected '.*'\(id:(\d+)\) reason 'reasonmsg=.*'$")
 
 proc = subprocess.Popen(['docker', 'logs', 'ts', '-f'],stdout=subprocess.PIPE)
 
-connected = list()
 while True:
   line = proc.stdout.readline().decode('utf-8')
   if not line:
     break
   connMatch = connRegex.search(line)
   if connMatch:
-    print('add')
-    id = connMatch.group(1)
-    print(id)
-    connected.append(id)
-    print(connected)
+    new_id = connMatch.group(1)
+    connected.append(new_id)
+    updateLed()
+
 
   discMatch = discRegex.search(line)
   if discMatch:
-    print('delete')
-    id = discMatch.group(1)
-    print(id)
-    if (id in connected):
-      connected.remove(id)
-    print(connected)
+    new_id = discMatch.group(1)
+    if (new_id in connected):
+      connected.remove(new_id)
+    updateLed()
